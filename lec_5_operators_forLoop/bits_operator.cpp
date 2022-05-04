d5c601bbdcfe1a9d6a55b5cdc753daa43d134a5a
alok <alok60541@gmail.com>
2022-05-04 23:33:01 +0530

    lec_5_operators_forLoop saved

---
+++ lec_5_operators_forLoop/bits_operator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=4;
+    int b=6;
+    int c= a++ + ++b + ++a + ++a;
+    cout<<c<<endl;
+    cout<< a<<endl;
+    cout<< b<<endl;
+
+//     cout<<a&b<<endl;
+//     cout<<a&b<<endl;
+//     cout<<a<<endl;
+//     cout<<a^b<<endl;
+
+
+}
